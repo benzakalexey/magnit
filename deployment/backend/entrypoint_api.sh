a22e1a220e002117429ef9850afbc1c0b886706d
Vasilevich Denis <rudevelooper@gmail.com>
2023-04-13 22:40:35 +0600

    issues

--- deployment/backend/entrypoint_api.sh
+++ deployment/backend/entrypoint_api.sh
@@ -7,7 +7,7 @@ if [ -z "$API_PORT" ]; then
   API_PORT=8082
 fi
 # shellcheck disable=SC2089
-GUNICORN_CMD_ARGS="--bind=0.0.0.0:$API_PORT --workers=1 --log-level $API_LOG_LEVEL --log-file - --forwarded-allow-ip '*' --proxy-allow-from '*' --timeout 300"
+GUNICORN_CMD_ARGS="--bind=0.0.0.0:$API_PORT --workers=1 --log-level $API_LOG_LEVEL --log-file - --forwarded-allow-ip '*' --proxy-allow-from '*' --timeout 300 --preload"
 # shellcheck disable=SC2090
 export GUNICORN_CMD_ARGS
 
