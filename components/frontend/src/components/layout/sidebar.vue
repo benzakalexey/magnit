c337bf35bdb18f5679373a1cd7d46cb80bf6a936
Vasilevich Denis <rudevelooper@gmail.com>
2024-01-10 18:38:57 +0600

    - add single link drivers - add role audit

--- components/frontend/src/components/layout/sidebar.vue
+++ components/frontend/src/components/layout/sidebar.vue
@@ -37,8 +37,10 @@ const links = {
     users: ['Супервайзер'],
     contragents: ['Супервайзер'],
     polygons: ['Супервайзер'],
+    drivers_single: ['Логист', 'Аналитик тонаров', 'Редактор тонаров'],
     drivers: ['Супервайзер'],
     contracts: ['Супервайзер'],
+    catalog: ['Супервайзер'],
 };
 
 </script>
@@ -128,7 +130,23 @@ const links = {
                     </router-link>
                 </li>
 
-                <li class="menu">
+                <li v-show="links.drivers_single.includes(store.state.AuthModule.credentials.user_role)" class="menu">
+                    <router-link to="/drivers" class="dropdown-toggle" @click="toggleMobileMenu">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-users">
+                                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="9" cy="7" r="4"></circle>
+                                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                            </svg>
+                            <span>{{ $t('drivers') }}</span>
+                        </div>
+                    </router-link>
+                </li>
+
+                <li v-show="links.catalog.includes(store.state.AuthModule.credentials.user_role)" class="menu">
                     <a class="dropdown-toggle" data-bs-toggle="collapse" data-bs-target="#components"
                         aria-controls="components" aria-expanded="false">
                         <div class="">
