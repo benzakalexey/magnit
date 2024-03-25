95631598dfc0915f6251e76a1eff86146d77a745
Denis Vasilevich <rudevelooper@gmail.com>
2024-03-25 21:33:12 +0600

    limit max netto

--- components/frontend/src/scripts/weight_corrector.js
+++ components/frontend/src/scripts/weight_corrector.js
@@ -2,6 +2,7 @@ const NETTO_DELTA = 30;
 const WEIGHT_STEP = 20;
 const CHUNK_SIZE = 20;
 const TOTAL_DIFF = 20000;
+const MAX_NETTO = 15000
 
 function set_netto(visits, netto, tonar = true) {
     if (netto % 20 !== 0) {
@@ -111,6 +112,9 @@ function changeWithMaxEffect(visits, target) {
             effectsByTrucks[minEffectVisit.netto - maxEffectVisit.netto] = maxEffectVisit
         } else {
             var maxNetto = maxEffectVisit.max_weight - maxEffectVisit.tara
+            if (maxNetto > MAX_NETTO) {
+                maxNetto = MAX_NETTO
+            }
             effectsByTrucks[maxNetto - maxEffectVisit.netto] = maxEffectVisit
         }
 
